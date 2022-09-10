acaefd335886590b35f134b06ef538b1041633f4
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2022-09-10 14:16:17 +0700

    Create Day_7.py

---
+++ 30_Days_of_Code/Day_7.py
@@ -0,0 +1,5 @@
+# Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+if __name__ == '__main__':
+  N = int(input().strip()) # the size of an array
+  A = list(map(int, intput().rstrip().split()))
+  print(*A[::-1], sep=' ')
