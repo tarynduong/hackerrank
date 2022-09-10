749dcdc3c8cae3067f6416f08a0a3a2eb86ad536
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2022-09-10 12:09:19 +0700

    Update Day_3.py

--- 30_Days_of_Code/Day_3.py
+++ 30_Days_of_Code/Day_3.py
@@ -1,9 +1,9 @@
 """
 Given an integer, , perform the following conditional actions:
 If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 20, print Weird
+If  is even and greater than 20, print Not Weird
 Complete the stub code provided in your editor to print whether or not  is weird.
 """
 if __name__ == '__main__':
