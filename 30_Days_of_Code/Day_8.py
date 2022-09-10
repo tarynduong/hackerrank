f2a793044f508609695755109851512d2c9388d5
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2022-09-10 14:30:17 +0700

    Create Day_8.py

---
+++ 30_Days_of_Code/Day_8.py
@@ -0,0 +1,29 @@
+"""
+The first line contains an integer, n, denoting the number of entries in the phone book.
+Each of the n subsequent lines describes an entry in the form of 2 space-separated values on a single line. The first value is a friend's name,
+and the second value is an 8-digit phone number.
+After the  lines of phone book entries, there are an unknown number of lines of queries.
+Each line (query) contains a name to look up, and you must continue reading lines until there is no more input.
+Note: Names consist of lowercase English alphabetic letters and are first names only.
+
+We add the following n=3 (Key, Value) pairs to our map so it looks like this:
+phoneBook = {(same, 1934769295), (harry, 42886197490), (kevin, 07365728744)}
+Then we process each query and print key=value if the queried key is found in the map; otherwise, print Not found.
+Query 0: sam
+Sam is one of the keys in our dictionary, so we print sam=1934769295.
+Query 1: edward
+Edward is not one of the keys in our dictionary, so we print Not found.
+"""
+n = int(input())
+phoneBook = {}
+
+for i in range(n):
+  entry = input().split()
+  phoneBook[entry[0]] = entry[1]
+
+for j in range(n):
+  name = str(input())
+  if name not in phoneBook:
+    print('Not found')
+  else:
+    print(name + '=' + str(phoneBook[nane]))
