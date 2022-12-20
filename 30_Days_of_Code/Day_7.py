8c2cbd88e8d6392572a4f95235529f46007f5bf7
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2022-12-20 09:40:32 +0700

    Update Day_7.py

--- 30_Days_of_Code/Day_7.py
+++ 30_Days_of_Code/Day_7.py
@@ -1,5 +1,5 @@
 # Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
 if __name__ == '__main__':
   N = int(input().strip()) # the size of an array
-  A = list(map(int, input().rstrip().split()))
-  print(*A[::-1], sep=' ')
+  A = list(map(int, input().rstrip().split())) # enter integers as array's elements, for split() by default any whitespace is a separator
+  print(*A[::-1], sep=' ') # with * the output will be in the form of a sequence of integers
