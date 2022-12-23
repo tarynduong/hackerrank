76f663838b86de4f0b7ac3502f5c999971b8f236
Taryn Duong <85856280+tarynduong@users.noreply.github.com>
2022-12-23 12:06:47 +0700

    Update Day_0.py

--- 10_Days_of_Statistics/Day_0.py
+++ 10_Days_of_Statistics/Day_0.py
@@ -1,3 +1,4 @@
+# Calculate mean, median, mode
 import numpy as np
 from scipy import stats
 
@@ -6,3 +7,15 @@ numbers = list(map(int, input().split()))
 print(np.mean(numbers))
 print(np.median(numbers))
 print(int(stats.mode(numbers)[0]))
+
+# Calculate weighted mean
+def weightedMean(X, W):
+  # Write your code here
+  new = [x * w for x, w in zip(X, W)]
+  print(round(sum(new) / sum(W), 1))
+        
+if __name__ == '__main__':
+  n = int(input().strip())
+  vals = list(map(int, input().rstrip().split()))
+  weights = list(map(int, input().rstrip().split()))
+  weightedMean(vals, weights)
